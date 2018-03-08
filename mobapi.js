c02deceeb45233c57952b940b28ceab6b266a32f
matsueT5374 <mishima001@users.noreply.github.com>
2018-03-08 14:11:06 +0900

    Update mobapi.js

--- mobapi.js
+++ mobapi.js
@@ -1,46 +1,73 @@
-(function getJSON() {
-
-    window.alert('①◇');
-
-let http = require('http');
-
-    window.alert('②');
+$(function () {
+    //検索ボタンをクリックされたときに実行
+    $("#search_btn").click(function () {
+        //入力値をセット
 
 
-const URL = 'http://qiita.com/kazuhikoyamashita/items/273692ccbdf8c0950a71.json';
 
-    window.alert('③');
+        window.alert('クリック' + param);
 
 
-http.get(URL, (res) => {
+        var httpObj = new XMLHttpRequest();
+        var httpObj.open("get", "./js/group.json", true);
 
-    window.alert('④');
+        httpObj.onload = function(){
+                var myData = JSON.parse(this.responseText);
+                var txt = "";
+                for (var i=0; i<myData.length; i++){
+                   var option = document.createElement("option");
+                    option.innerText = myData[i].name;
+                    option.value=myData[i].value;
+                    document.getElementById("group_select").appendChild(option); 
+                }
 
-  let body = '';
+            }
 
-    window.alert('⑤');
+       httpObj.send(null);
 
-  res.setEncoding('utf8');
 
-    window.alert('⑥');
 
-  res.on('data', (chunk) => {
-      body += chunk;
-  });
+        var param = {zipcode: $('#zipcode').val()}
+        //zipcloudのAPIのURL
+        var send_url = "http://zipcloud.ibsnet.co.jp/api/search";
 
-    window.alert('⑦');
 
-  res.on('end', (res) => {
-      res = JSON.parse(body);
-
-    window.alert('⑧' + res);
-
-      console.log(res);
-  });
-}).on('error', (e) => {
-
-    window.alert('⑨' + e.message);
-
-  console.log(e.message); //エラー時
+        $.ajax({
+            type: "GET",
+            cache: false,
+            data: param,
+            url: send_url,
+            dataType: "jsonp",
+            success: function (res) {
+                //結果によって処理を振り分ける
+                if (res.status == 200) {
+                    //処理が成功したとき
+                    //該当する住所を表示
+                    var html = '';
+                    for (var i = 0; i < res.results.length; i++) {
+                        var result = res.results[i];
+                        console.log(res.results);
+                        html += '<h2>住所' + (i + 1) + '</h2>';
+                        html += '<div>都道府県コード：' + result.prefcode + '</div>';
+                        html += '<div>都道府県：' + result.address1 + '</div>';
+                        html += '<div>市区町村：' + result.address2 + '</div>';
+                        html += '<div>町域：' + result.address3 + '</div>';
+                        html += '<div>都道府県(カナ)：' + result.kana1 + '</div>';
+                        html += '<div>市区町村(カナ)：' + result.kana1 + '</div>';
+                        html += '<div>町域(カナ)：' + result.kana1 + '</div>';
+                    }
+                    $('#zip_result').html(html);
+                } else {
+                    //エラーだった時
+                    //エラー内容を表示
+                    window.alert('失敗' + res.message);
+                    $('#zip_result').html(res.message);
+ 
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log(XMLHttpRequest);
+            }
+        });
+    });
 });
-})();
