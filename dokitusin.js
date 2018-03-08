95e8f8d6f5c53d062c1befa7af421a3fb7969dc5
matsueT5374 <mishima001@users.noreply.github.com>
2018-03-08 14:57:59 +0900

    Update dokitusin.js

--- dokitusin.js
+++ dokitusin.js
@@ -1,8 +1,15 @@
-(function asyncSend() {
-
-    window.alert("asyncSend");
-
-var req = new XMLHttpRequest();
-
+function dispMsg(str){
+  document.write("<p>");
+  document.write(str);
+  document.write("</p>");
 }
-)
+
+dispMsg("Hello");
+document.write("<p>Good morning</p>");
+dispMsg("bye");
+
+dispCopyright();
+
+function dispCopyright(){
+  document.write("<p>Copyright (C) 2009 xxx. All Rights Reserved.</p>");
+}
